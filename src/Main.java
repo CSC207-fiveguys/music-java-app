619c25d5209b7b4a63a8fdf731e50b073c123368
Bryan Chen <bryanjj.chen@mail.utoronto.ca>
2023-09-30 21:38:53 -0400

    parsed response into JSON file

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpClient;
 import java.io.IOException;
+import org.json.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,22 +18,24 @@ public class Main {
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://accounts.spotify.com/api/token"
-                                + "?" + "grant_type=client_credentials"))
+                        + "?" + "grant_type=client_credentials"))
                 .header("Authorization", "Basic " + authB64Encoded)
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .method("POST", HttpRequest.BodyPublishers.noBody())
                 .build();
 
         HttpResponse<String> response = null;
+        JSONObject jsonResponse = null;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            jsonResponse = new JSONObject(response.body());
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        assert response != null;
-        System.out.println(response.body());
+        assert jsonResponse != null;
+        System.out.println(jsonResponse);
     }
 }
