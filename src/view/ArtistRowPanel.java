f3a1cb4acdd37d6ce8cf1b32a725aebe7ac95a9d
ibrahimmalik <ibrahimmohammadmalik@icloud.com>
2023-10-23 12:43:57 -0400

    Added comments to ensure understanding of written code

--- src/view/ArtistRowPanel.java
+++ src/view/ArtistRowPanel.java
@@ -12,10 +12,15 @@ class ArtistRowPanel extends JPanel {
 
         Image image;
 
+        // Creating a panel display for the artist class, displaying the name in the panel or displaying
+        // an error image if there is an issue
         try {
+            // Obtains an image of the selected artist from the API to display as part of the panel
             URL url = new URL(imageURL);
             image = ImageIO.read(url);
         } catch (Exception e) {
+            // In case an image is not able to save, the photo of the artist will be replaced
+            // by a generic error image saved outside the src folder
             File imgageFile = new File("error-image-generic.png");
             try {
                 image = ImageIO.read(imgageFile);
@@ -24,10 +29,12 @@ class ArtistRowPanel extends JPanel {
             }
         }
 
+        // Sets the image obtained through the try-catch and sets it as the icon for the current artist
         image = image.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
         ImageIcon icon = new ImageIcon(image);
         name.setIcon(icon);
 
+        // Other variables for the panel display, showing the name and follower count alongside the photo
         this.add(name);
         this.add(followers);
     }
