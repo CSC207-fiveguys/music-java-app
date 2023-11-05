82e348456fe414dbdb1929241046b4cb0adab452
HussainE4 <hussain.elahi@mail.utoronto.ca>
2023-11-05 18:57:35 -0500

    Updated the Data Access Object to store actual Users instead of just the usernames and passwords. Changed method calls to reflect these changes

--- src/data_access/FakeDAO.java
+++ src/data_access/FakeDAO.java
@@ -1,27 +1,33 @@
 package data_access;
 
+import entities.User;
+import entities.UserFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class FakeDAO {
 
-    private final Map<String, String> users;
+    private final Map<String, User> users;
 
-    public FakeDAO() {
+    private UserFactory userFactory;
+
+    public FakeDAO(UserFactory userFactory) {
         users = new HashMap<>();
-        // add 2 fake users
-        users.put("_Jason", "pass123");
-        users.put("bob445", "bananaPASS");
+        // Create 2 fake users and store them in users
+        User fake_user1 = userFactory.create("_Jason", "pass123");
+        User fake_user2 = userFactory.create("bob445", "bananaPASS");
+        users.put(fake_user1.getName(), fake_user1);
+        users.put(fake_user2.getName(), fake_user2);
+
+        // SOMETHING WE NEED TO FIGURE OUT: HOW WE'RE STORING THE USERS AND OTHER DATA PERSISTENCE
     }
 
-    /**
-     * @return password or null if username not found
-     */
-    public String getPassword(String username) {
-        return users.get(username);
-    }
 
-    public void saveUser(String username, String password) {
-        users.put(username, password);
-    }
+    public User getUser(String username){return users.get(username);}
+
+    public void saveUser(User user){users.put(user.getName(), user);}
+
+    public boolean exists(String username){return users.containsKey(username);}
+
 }
