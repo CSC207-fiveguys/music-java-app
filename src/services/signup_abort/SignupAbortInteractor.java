08ef0d5411fcd1eae7ea380d37605e7bfaa86139
bryan <bryanjj.chen@mail.utoronto.ca>
2023-11-06 12:06:29 -0500

    implemented SignupAbortInteractor

--- src/services/signup_abort/SignupAbortInteractor.java
+++ src/services/signup_abort/SignupAbortInteractor.java
@@ -1,4 +1,20 @@
 package services.signup_abort;
 
-public class SignupAbortInteractor {
+public class SignupAbortInteractor implements SignupAbortInputBoundary{
+    private SignupAbortOutputBoundary userPresenter;
+    private SignupAbortUserDataAccessInterface userDataAccess;
+
+    public SignupAbortInteractor(
+            SignupAbortOutputBoundary outputBoundary,
+            SignupAbortUserDataAccessInterface userDataAccess
+    ) {
+        this.userPresenter = outputBoundary;
+        this.userDataAccess = userDataAccess;
+    }
+
+    @Override
+    public void initiateLoginProcess() {
+        SignupAbortOutputData signupAbortOutputData = new SignupAbortOutputData();
+        userPresenter.presentLoginView(signupAbortOutputData);
+    }
 }
