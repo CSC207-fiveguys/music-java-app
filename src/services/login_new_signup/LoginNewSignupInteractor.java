197aacfccc7874964fb687eac9c1b594229a7676
bryan <bryanjj.chen@mail.utoronto.ca>
2023-11-07 06:25:38 -0500

    removed the usage of data access interface as was unneeded

--- src/services/login_new_signup/LoginNewSignupInteractor.java
+++ src/services/login_new_signup/LoginNewSignupInteractor.java
@@ -2,16 +2,12 @@ package services.login_new_signup;
 
 public class LoginNewSignupInteractor implements LoginNewSignupInputBoundary{
     final LoginNewSignupOutputBoundary userPresenter;
-    final LoginNewSignupUserDataAccessInterface userDataAccess;
-    public LoginNewSignupInteractor(LoginNewSignupOutputBoundary loginNewSignupOutputBoundary,
-                                    LoginNewSignupUserDataAccessInterface userDataAccess) {
+    public LoginNewSignupInteractor(LoginNewSignupOutputBoundary loginNewSignupOutputBoundary) {
         this.userPresenter = loginNewSignupOutputBoundary;
-        this.userDataAccess = userDataAccess;
     }
 
     @Override
-    public void initiateSignupProcess() {
-        LoginNewSignupOutputData loginNewSignupOutputData = new LoginNewSignupOutputData();
-        userPresenter.presentSignupView(loginNewSignupOutputData);
+    public void execute() {
+        userPresenter.presentSignupView();
     }
 }
