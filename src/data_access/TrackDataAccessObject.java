c51a968d112bf110c658f2034ec7d2845073168c
HussainE4 <hussain.elahi@mail.utoronto.ca>
2023-11-08 16:11:00 -0500

    Added Artist and Track Data Access Objects. These contain a dictionary with the String ids as the keys and the actual Object as the value. Implemented basic methods (get, save, exists)

--- src/data_access/TrackDataAccessObject.java
+++ src/data_access/TrackDataAccessObject.java
@@ -1,4 +1,36 @@
 package data_access;
 
+import entities.Track;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrackDataAccessObject {
+
+    private final Map<String, Track> tracks;
+
+    public TrackDataAccessObject(){
+        // The DataAccessObject stores the track ids and Track objects of all the tracks that we
+        // have gotten from the Spotify API so far. This is to reduce API calls and prevent
+        // having duplicate objects for the same track.
+        // This data is kept in a json file
+        tracks = new HashMap<>();
+        // TODO: Read from the json file that stores Track Data
+    }
+
+    public Track getTrack(String trackID){return tracks.get(trackID);}
+
+    public void saveTrack(String trackID, Track track){
+        // Add the track to the dictionary and then update the json file
+        tracks.put(trackID, track);
+        save();
+    }
+
+    public boolean exists(String trackID){return tracks.containsKey(trackID);}
+
+    private void save(){
+        // Write the current Track data in tracks to the json file
+        // TODO Implement this method
+    }
+
 }
