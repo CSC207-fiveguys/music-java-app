d55d9a6d06ab4b90a1a15e2598cb3857d4316083
umututku03 <egemen.umut@mail.utoronto.ca>
2023-11-07 22:58:38 -0500

    Implementing LoginComplete Use-Case Interactor.

--- src/services/login_complete/LoginCompleteInteractor.java
+++ src/services/login_complete/LoginCompleteInteractor.java
@@ -1,4 +1,35 @@
 package services.login_complete;
 
-public class LoginCompleteInteractor {
+import entities.User;
+
+public class LoginCompleteInteractor implements LoginCompleteInputBoundary {
+    final LoginCompleteUserDataAccessInterface userDataAccessObject;
+    final LoginCompleteOutputBoundary loginCompletePresenter;
+
+    public LoginCompleteInteractor(LoginCompleteUserDataAccessInterface userDataAccessObject, LoginCompleteOutputBoundary loginCompletePresenter) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.loginCompletePresenter = loginCompletePresenter;
+    }
+
+
+    @Override
+    public void execute(LoginCompleteInputData loginCompleteInputData) {
+        String username = loginCompleteInputData.getUsername();
+        String password = loginCompleteInputData.getPassword();
+        if (!userDataAccessObject.existsByName(username)){
+            loginCompletePresenter.prepareFailView(username + ": Account does not exist.");
+        } else {
+            String pwd = userDataAccessObject.get(username).getPassword();
+            if(!password.equals(pwd)){
+                loginCompletePresenter.prepareFailView("Incorrect password for " + username + ".");
+            } else {
+
+                User user = userDataAccessObject.get(loginCompleteInputData.getUsername());
+
+                LoginCompleteOutputData loginCompleteOutputData = new LoginCompleteOutputData(user.getUsername(),
+                        false);
+                loginCompletePresenter.prepareSuccessView(loginCompleteOutputData);
+            }
+        }
+    }
 }
