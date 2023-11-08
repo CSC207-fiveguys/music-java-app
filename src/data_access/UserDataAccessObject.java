f1552104a5d27010e57b9428fe0f390c8b3bcebf
HussainE4 <hussain.elahi@mail.utoronto.ca>
2023-11-08 18:10:45 -0500

    Reformatted code to be in line with Google Java Style Guide

--- src/data_access/UserDataAccessObject.java
+++ src/data_access/UserDataAccessObject.java
@@ -8,36 +8,41 @@ import java.util.Map;
 
 public class UserDataAccessObject {
 
-    private final Map<String, User> users;
+  private final Map<String, User> users;
 
-    private final UserFactory userFactory;
+  private final UserFactory userFactory;
 
-    public UserDataAccessObject(UserFactory userFactory) {
-        this.userFactory = userFactory;
-        users = new HashMap<>();
-        // The DataAccessObject stores the usernames and User objects of all the users so far
-        // This data is kept in a json file
-        // Create 2 fake users and store them in users
-        User fake_user1 = userFactory.create("_Jason", "pass123");
-        User fake_user2 = userFactory.create("bob445", "bananaPASS");
-        users.put(fake_user1.getUsername(), fake_user1);
-        users.put(fake_user2.getUsername(), fake_user2);
+  public UserDataAccessObject(UserFactory userFactory) {
+    this.userFactory = userFactory;
+    users = new HashMap<>();
+    // The DataAccessObject stores the usernames and User objects of all the users so far
+    // This data is kept in a json file
+    // Create 2 fake users and store them in users
+    User fake_user1 = userFactory.create("_Jason", "pass123");
+    User fake_user2 = userFactory.create("bob445", "bananaPASS");
+    users.put(fake_user1.getUsername(), fake_user1);
+    users.put(fake_user2.getUsername(), fake_user2);
 
-        // TODO: Read from the json file that stores User information
-    }
-    public User getUser(String username){return users.get(username);}
+    // TODO: Read from the json file that stores User information
+  }
 
-    public void saveUser(User user){
-        // Add the user to the dictionary and then update the json file
-        users.put(user.getUsername(), user);
-        save();
-    }
+  public User getUser(String username) {
+    return users.get(username);
+  }
 
-    public boolean exists(String username){return users.containsKey(username);}
+  public void saveUser(User user) {
+    // Add the user to the dictionary and then update the json file
+    users.put(user.getUsername(), user);
+    save();
+  }
 
-    private void save(){
-        // Write the user data from users to the json file
-        // TODO Implement this method
-    }
+  public boolean exists(String username) {
+    return users.containsKey(username);
+  }
+
+  private void save() {
+    // Write the user data from users to the json file
+    // TODO Implement this method
+  }
 
 }
