8be1f8c680fafb9d8cfc7df8de1d8c8aa70d0f18
umututku03 <egemen.umut@mail.utoronto.ca>
2023-11-13 10:21:32 -0500

    Reformatting code w.r.t Google Java Style Guide

--- src/view/logged_out/LoginView.java
+++ src/view/logged_out/LoginView.java
@@ -10,56 +10,58 @@ import java.beans.PropertyChangeListener;
 
 public class LoginView extends JPanel implements PropertyChangeListener {
 
-    private final LoginViewModel loginViewModel;
-    private final LoginCompleteController loginCompleteController;
-    private final LoginNewSignupController loginNewSignupController;
-    private final JLabel currentError;
-    private final JTextField usernameInputField;
-    private final JPasswordField passwordInputField;
-    private final JButton loginButton;
-    private final JButton signupButton;
+  private final LoginViewModel loginViewModel;
+  private final LoginCompleteController loginCompleteController;
+  private final LoginNewSignupController loginNewSignupController;
+  private final JLabel currentError;
+  private final JTextField usernameInputField;
+  private final JPasswordField passwordInputField;
+  private final JButton loginButton;
+  private final JButton signupButton;
 
-    public LoginView(LoginViewModel loginViewModel,
-                     LoginCompleteController loginCompleteController,
-                     LoginNewSignupController loginNewSignupController) {
+  public LoginView(LoginViewModel loginViewModel,
+      LoginCompleteController loginCompleteController,
+      LoginNewSignupController loginNewSignupController) {
 
-        this.loginViewModel = loginViewModel;
-        loginViewModel.addPropertyChangeListener(this);
-        this.loginCompleteController = loginCompleteController;
-        this.loginNewSignupController = loginNewSignupController;
+    this.loginViewModel = loginViewModel;
+    loginViewModel.addPropertyChangeListener(this);
+    this.loginCompleteController = loginCompleteController;
+    this.loginNewSignupController = loginNewSignupController;
 
-        currentError = new JLabel(loginViewModel.state.currentError);
-        add(currentError);
+    currentError = new JLabel(loginViewModel.state.currentError);
+    add(currentError);
 
-        usernameInputField = new JTextField(15);
-        LabelTextPanel usernamePanel = new LabelTextPanel(new JLabel(loginViewModel.state.usernameInputFieldText), usernameInputField);
-        add(usernamePanel);
+    usernameInputField = new JTextField(15);
+    LabelTextPanel usernamePanel = new LabelTextPanel(
+        new JLabel(loginViewModel.state.usernameInputFieldText), usernameInputField);
+    add(usernamePanel);
 
-        passwordInputField = new JPasswordField(15);
-        LabelTextPanel passwordPanel = new LabelTextPanel(new JLabel(loginViewModel.state.passwordInputFieldText), passwordInputField);
-        add(passwordPanel);
+    passwordInputField = new JPasswordField(15);
+    LabelTextPanel passwordPanel = new LabelTextPanel(
+        new JLabel(loginViewModel.state.passwordInputFieldText), passwordInputField);
+    add(passwordPanel);
 
-        loginButton = new JButton(loginViewModel.state.loginButtonText);
-        add(loginButton);
-        loginButton.addActionListener(e -> loginButtonPressed());
+    loginButton = new JButton(loginViewModel.state.loginButtonText);
+    add(loginButton);
+    loginButton.addActionListener(e -> loginButtonPressed());
 
-        signupButton = new JButton(loginViewModel.state.signupButtonText);
-        add(signupButton);
-        loginButton.addActionListener(e -> signupButtonPressed());
-    }
+    signupButton = new JButton(loginViewModel.state.signupButtonText);
+    add(signupButton);
+    loginButton.addActionListener(e -> signupButtonPressed());
+  }
 
-    private void loginButtonPressed() {
-        loginCompleteController.execute(loginViewModel.state.usernameInputFieldText,
-            loginViewModel.state.passwordInputFieldText);
-    }
+  private void loginButtonPressed() {
+    loginCompleteController.execute(loginViewModel.state.usernameInputFieldText,
+        loginViewModel.state.passwordInputFieldText);
+  }
 
-    private void signupButtonPressed() {
+  private void signupButtonPressed() {
 //        loginNewSignupController.execute(); todo
-    }
+  }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-        // handle property changes fired from signupViewModel
-    }
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    // handle property changes fired from signupViewModel
+  }
 }
 
