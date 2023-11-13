77eb8775bd3f2c417719ad12996cef26836aa1ce
bryan <bryanjj.chen@mail.utoronto.ca>
2023-11-13 10:32:46 -0500

    changed indentation

--- src/view/logged_out/LoginView.java
+++ src/view/logged_out/LoginView.java
@@ -10,54 +10,54 @@ import java.beans.PropertyChangeListener;
 
 public class LoginView extends JPanel implements PropertyChangeListener {
 
-  private final LoginViewModel loginViewModel;
-  private final LoginCompleteController loginCompleteController;
-  private final LoginNewSignupController loginNewSignupController;
+    private final LoginViewModel loginViewModel;
+    private final LoginCompleteController loginCompleteController;
+    private final LoginNewSignupController loginNewSignupController;
 
-  private final JTextField usernameInputField;
-  private final JPasswordField passwordInputField;
-  private final JButton loginButton;
-  private final JButton signupButton;
+    private final JTextField usernameInputField;
+    private final JPasswordField passwordInputField;
+    private final JButton loginButton;
+    private final JButton signupButton;
 
-  public LoginView(LoginViewModel loginViewModel,
-      LoginCompleteController loginCompleteController,
-      LoginNewSignupController loginNewSignupController) {
+    public LoginView(LoginViewModel loginViewModel,
+        LoginCompleteController loginCompleteController,
+        LoginNewSignupController loginNewSignupController) {
 
-    this.loginViewModel = loginViewModel;
-    loginViewModel.addPropertyChangeListener(this);
-    this.loginCompleteController = loginCompleteController;
-    this.loginNewSignupController = loginNewSignupController;
+        this.loginViewModel = loginViewModel;
+        loginViewModel.addPropertyChangeListener(this);
+        this.loginCompleteController = loginCompleteController;
+        this.loginNewSignupController = loginNewSignupController;
 
-    usernameInputField = new JTextField(15);
-    LabelTextPanel usernamePanel = new LabelTextPanel(
-        new JLabel(loginViewModel.state.usernameInputFieldText), usernameInputField);
-    add(usernamePanel);
+        usernameInputField = new JTextField(15);
+        LabelTextPanel usernamePanel = new LabelTextPanel(
+            new JLabel(loginViewModel.state.usernameInputFieldText), usernameInputField);
+        add(usernamePanel);
 
-    passwordInputField = new JPasswordField(15);
-    LabelTextPanel passwordPanel = new LabelTextPanel(
-        new JLabel(loginViewModel.state.passwordInputFieldText), passwordInputField);
-    add(passwordPanel);
+        passwordInputField = new JPasswordField(15);
+        LabelTextPanel passwordPanel = new LabelTextPanel(
+            new JLabel(loginViewModel.state.passwordInputFieldText), passwordInputField);
+        add(passwordPanel);
 
-    loginButton = new JButton(loginViewModel.state.loginButtonText);
-    add(loginButton);
-    loginButton.addActionListener(e -> loginButtonPressed());
+        loginButton = new JButton(loginViewModel.state.loginButtonText);
+        add(loginButton);
+        loginButton.addActionListener(e -> loginButtonPressed());
 
-    signupButton = new JButton(loginViewModel.state.signupButtonText);
-    add(signupButton);
-    signupButton.addActionListener(e -> signupButtonPressed());
-  }
+        signupButton = new JButton(loginViewModel.state.signupButtonText);
+        add(signupButton);
+        signupButton.addActionListener(e -> signupButtonPressed());
+    }
 
-  private void loginButtonPressed() {
+    private void loginButtonPressed() {
 //        loginCompleteController.execute(); todo
-  }
+    }
 
-  private void signupButtonPressed() {
-    loginNewSignupController.execute();
-  }
+    private void signupButtonPressed() {
+        loginNewSignupController.execute();
+    }
 
-  @Override
-  public void propertyChange(PropertyChangeEvent evt) {
-    // handle property changes fired from signupViewModel
-  }
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // handle property changes fired from signupViewModel
+    }
 }
 
