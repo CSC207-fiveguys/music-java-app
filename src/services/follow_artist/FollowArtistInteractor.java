aee7c1fc09c7647691cb3b69829e56d2dc76c92e
umututku03 <egemen.umut@mail.utoronto.ca>
2023-12-01 22:20:24 -0500

    Initial files

---
+++ src/services/follow_artist/FollowArtistInteractor.java
@@ -0,0 +1,9 @@
+package services.follow_artist;
+
+public class FollowArtistInteractor implements FollowArtistInputBoundary {
+
+  @Override
+  public void execute(FollowArtistInputData followArtistInputData) {
+
+  }
+}
---
+++ src/services/follow_artist/FollowArtistOutputData.java
@@ -0,0 +1,5 @@
+package services.follow_artist;
+
+public class FollowArtistOutputData {
+
+}
