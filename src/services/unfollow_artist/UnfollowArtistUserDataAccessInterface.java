688a867d13f725be3f92aba0208223a80f96f23b
umututku03 <egemen.umut@mail.utoronto.ca>
2023-12-02 13:14:43 -0500

    Removing unnecessary dependencies

--- src/services/unfollow_artist/UnfollowArtistUserDataAccessInterface.java
+++ src/services/unfollow_artist/UnfollowArtistUserDataAccessInterface.java
@@ -7,13 +7,6 @@ public interface UnfollowArtistUserDataAccessInterface {
 
   public User getUser(String username);
 
-  public void saveUser(User user);
-
-  public boolean exists(String username);
-
-  private void save() {
-  }
-
   public ArrayList<String> searchUser(String query);
 
 }
