b3eae79897eec823a615e519a5e42e37b97398e9
HussainE4 <hussain.elahi@mail.utoronto.ca>
2023-12-03 15:20:34 -0500

    Removed unused imports

--- src/services/view_playlist/ViewPlaylistDataAccessInterface.java
+++ src/services/view_playlist/ViewPlaylistDataAccessInterface.java
@@ -1,9 +1,7 @@
 package services.view_playlist;
 
 import entities.Playlist;
-import entities.Track;
 import entities.User;
-import java.util.ArrayList;
 
 public interface ViewPlaylistDataAccessInterface {
 
