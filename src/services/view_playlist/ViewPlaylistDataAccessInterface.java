9e8cd1b1cfca74c130330d25e88dda08962cc7fa
HussainE4 <hussain.elahi@mail.utoronto.ca>
2023-12-03 15:20:19 -0500

    Fixed bug with View Playlist

--- src/services/view_playlist/ViewPlaylistDataAccessInterface.java
+++ src/services/view_playlist/ViewPlaylistDataAccessInterface.java
@@ -2,10 +2,13 @@ package services.view_playlist;
 
 import entities.Playlist;
 import entities.Track;
+import entities.User;
 import java.util.ArrayList;
 
 public interface ViewPlaylistDataAccessInterface {
 
   Playlist getPlaylist(String playlistName, String username);
+
+  User getUser(String username);
 
 }
